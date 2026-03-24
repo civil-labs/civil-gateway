82f2538a08f9c94adaf406520a7b9a81c2a731d2
Jackson Arnold <jackson.n.arnold@gmail.com>
2026-03-23 20:28:04 -0400

    fix: healthcheckhandler

--- health.go
+++ health.go
@@ -5,10 +5,23 @@ import (
 	"net/http"
 )
 
-// HealthCheckHandler returns 200 for now. In the future may
+// HealthResponse is the JSON structure we return
+type HealthResponse struct {
+	Status       string `json:"status"`
+}
+
+// HealthCheckHandler returns 200 if we have backends, In the future may
 // do further health introspection to downstream services
 func HealthCheckHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+
+		// Check if there is anywhere to send tile server traffic to
+		ready := lb.IsReady()
+
+		// Prepare the response
+		resp := HealthResponse{
+			Status: "OK",
+		}
 
 		w.Header().Set("Content-Type", "application/json")
 
