257b931a7f349c0dc366f6cb4379d38b8eee8433
Jackson Arnold <jackson.n.arnold@gmail.com>
2026-05-07 11:52:21 -0400

    feat: update parcel APIs

---
+++ parcels.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+
+	publicparcelsv1 "github.com/civil-labs/civil-api-go/civil/public/parcels/v1"
+
+	meshparcelsv1 "github.com/civil-labs/civil-api-go/civil/mesh/parcels/v1"
+	meshparcelsv1connect "github.com/civil-labs/civil-api-go/civil/mesh/parcels/v1/parcelsv1connect"
+
+	"connectrpc.com/connect"
+)
+
+type ParcelServer struct {
+	dbReaderClient meshparcelsv1connect.ParcelsServiceClient
+	logger         *slog.Logger
+}
+
+func (s *ParcelServer) GetParcelsById(
+	ctx context.Context,
+	req *connect.Request[publicparcelsv1.GetParcelsByIdRequest],
+) (*connect.Response[publicparcelsv1.GetParcelsByIdResponse], error) {
+
+	s.logger.Debug("received GetParcels request")
+
+	meshReq := connect.NewRequest(&meshparcelsv1.GetParcelsByIdRequest{
+		ParcelIds:  req.Msg.ParcelIds,
+		LegalAsOf:  req.Msg.LegalAsOf,
+		SystemAsOf: req.Msg.SystemAsOf,
+	})
+
+	meshRes, err := s.dbReaderClient.GetParcelsById(ctx, meshReq)
+	if err != nil {
+		// ConnectRPC automatically handles wrapping standard gRPC error codes
+		// You might want to log the internal error here, but return a sanitized error to the public client
+		slog.Error("upstream GetParcesls request failed", slog.Any("error", err))
+		return nil, connect.NewError(connect.CodeInternal, err)
+	}
+
+	// Initialize a new map for the public response, sized to match the incoming data
+	publicParcels := make(map[string]*publicparcelsv1.Parcel, len(meshRes.Msg.Parcels))
+
+	// Iterate and manually map the fields
+	for id, meshParcel := range meshRes.Msg.Parcels {
+		if meshParcel == nil {
+			continue // Always good practice to guard against nil pointers in protobuf maps
+		}
+
+		publicParcels[id] = &publicparcelsv1.Parcel{
+			ParcelId:                 meshParcel.ParcelId,
+			Address:                  meshParcel.Address,
+			AddressId:                meshParcel.AddressId,
+			OwnerName:                meshParcel.OwnerName,
+			OwnerAddress:             meshParcel.OwnerAddress,
+			OwnerId:                  meshParcel.OwnerId,
+			LandAreaSqFt:             meshParcel.LandAreaSqFt,
+			LandUse:                  meshParcel.LandUse,
+			LandUseCode:              meshParcel.LandUseCode,
+			Neighborhood:             meshParcel.Neighborhood,
+			MarketArea:               meshParcel.MarketArea,
+			Zoning:                   meshParcel.Zoning,
+			ZoningCodes:              meshParcel.ZoningCodes,
+			MarketLandValue:          meshParcel.MarketLandValue,
+			AssessedLandValue:        meshParcel.AssessedLandValue,
+			MarketImprovementValue:   meshParcel.MarketImprovementValue,
+			AssessedImprovementValue: meshParcel.AssessedImprovementValue,
+
+			Affordances: &publicparcelsv1.ParcelAffordances{
+				MaxFar:         meshParcel.Affordances.MaxFar,
+				MinLotSizeSqFt: meshParcel.Affordances.MinLotSizeSqFt,
+				MaxHeightFt:    meshParcel.Affordances.MaxHeightFt,
+			},
+
+			ImprovementSummary: &publicparcelsv1.ParcelImprovementsSummary{
+				ImprovementIds:      meshParcel.ImprovementSummary.ImprovementIds,
+				TotalAreaSqFt:       meshParcel.ImprovementSummary.TotalAreaSqFt,
+				TotalBathrooms:      meshParcel.ImprovementSummary.TotalBathrooms,
+				TotalBedrooms:       meshParcel.ImprovementSummary.TotalBedrooms,
+				TotalUnits:          meshParcel.ImprovementSummary.TotalUnits,
+				OldestYearBuilt:     meshParcel.ImprovementSummary.OldestYearBuilt,
+				NewestYearBuilt:     meshParcel.ImprovementSummary.NewestYearBuilt,
+				LowestConditionNum:  meshParcel.ImprovementSummary.LowestConditionNum,
+				HighestConditionNum: meshParcel.ImprovementSummary.HighestConditionNum,
+			},
+
+			Properties: meshParcel.Properties,
+		}
+	}
+
+	publicRes := &publicparcelsv1.GetParcelsByIdResponse{
+		Parcels: publicParcels,
+	}
+	return connect.NewResponse(publicRes), nil
+}
+
+func (s *ParcelServer) UpdateParcel(
+	ctx context.Context,
+	req *connect.Request[publicparcelsv1.UpdateParcelRequest],
+) (*connect.Response[publicparcelsv1.UpdateParcelResponse], error) {
+
+	s.logger.Debug("received UpdateParcel request")
+
+	publicRes := &publicparcelsv1.UpdateParcelResponse{}
+
+	return connect.NewResponse(publicRes), nil
+}
+
+func (s *ParcelServer) GetCategoricalParcelStatsById(
+	_ context.Context,
+	req *connect.Request[publicparcelsv1.GetCategoricalParcelStatsByIdRequest],
+) (*connect.Response[publicparcelsv1.GetCategoricalParcelStatsByIdResponse], error) {
+	res := &publicparcelsv1.GetCategoricalParcelStatsByIdResponse{
+		Mode: "example",
+		UniqueValues: map[string]int32{
+			"value1": 12,
+			"value2": 13,
+		},
+	}
+	return connect.NewResponse(res), nil
+}
+
+func (s *ParcelServer) GetNumericalParcelStatsById(
+	_ context.Context,
+	req *connect.Request[publicparcelsv1.GetNumericalParcelStatsByIdRequest],
+) (*connect.Response[publicparcelsv1.GetNumericalParcelStatsByIdResponse], error) {
+	res := &publicparcelsv1.GetNumericalParcelStatsByIdResponse{
+		Mode:                    5,
+		Minimum:                 1,
+		Maximum:                 10,
+		Percentile_10:           1,
+		Percentile_20:           2,
+		Percentile_30:           3,
+		Percentile_40:           4,
+		Percentile_50:           5,
+		Percentile_60:           6,
+		Percentile_70:           7,
+		Percentile_80:           8,
+		Percentile_90:           9,
+		Percentile_100:          10,
+		Mean:                    5,
+		StandardDeviation:       2,
+		CoefficientOfDispersion: 3,
+	}
+	return connect.NewResponse(res), nil
+}
